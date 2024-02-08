101e8d00172fcfbbee0e8c87e2a3bcbf9162cc99
mohammad hamza <hamzarvp20wr10@gmail.com>
2024-02-08 18:10:27 +0500

    adding file

---
+++ twitter.py
@@ -0,0 +1,8 @@
+import re
+url=input("URL: ").strip()
+
+"""username = re.sub(r"^(https?://)?(www\.)?twitter\.com/","",url) #re.sub means to subsitute and it return a string its function looks like this re.sub(pattern,repl,string,count=0,flag=0)
+print("Username:",username)"""
+matches=re.search(r"^(?:https?://)?(?:www\.)?twitter\.com/([a-z0-9_]+)",url,re.IGNORECASE) # parenthesis captures the value ?: indicates value will not be captures its for ? this operator
+if matches:
+    print("Username:", matches.group(1))
